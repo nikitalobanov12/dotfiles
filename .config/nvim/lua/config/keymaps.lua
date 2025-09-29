c2ddb062e9fff9fac4bb76b30ac454650500b8b1
Nikita Lobanov <nikitaalobanovv@gmail.com>
2025-09-28 21:08:22 -0700

    replaced oil for yazi

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Navigation & File Management
-vim.keymap.set({ "n", "i" }, "<C-e>", "<CMD>Oil<CR>", { desc = "Open Oil file explorer" })
-vim.keymap.set("n", "rr", "<CMD>Oil<CR>", { desc = "Open Oil file explorer" })
+vim.keymap.set({ "n", "i" }, "<C-e>", "<CMD>Yazi<CR>", { desc = "Open Yazi file manager" })
+vim.keymap.set("n", "rr", "<CMD>Yazi<CR>", { desc = "Open Yazi file manager" })
 
 vim.keymap.set({ "i", "n" }, "<C-q>", function()
   Snacks.picker.grep({ cwd = vim.fn.getcwd() })
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 -- Plugins disabled in LazyVim
 return {
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvim-mini/mini.indentscope", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
 }
