ed800b8e724f3f2026baecb62fd9a5332c169f83
Nikita Lobanov <nikitaalobanovv@gmail.com>
2025-10-08 08:26:58 -0700

    vins

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,22 +7,22 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained", "BufEn
 })
 
 -- Force cursor shape after all plugins load
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		vim.defer_fn(function()
-			vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250"
-		end, 100)
-	end,
-	desc = "Set cursor shape after plugins load",
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	callback = function()
+-- 		vim.defer_fn(function()
+-- 			vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250"
+-- 		end, 100)
+-- 	end,
+-- 	desc = "Set cursor shape after plugins load",
+-- })
 
--- Maintain cursor shape on mode changes
-vim.api.nvim_create_autocmd("ModeChanged", {
-	callback = function()
-		vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250"
-	end,
-	desc = "Maintain cursor shape on mode change",
-})
+-- -- Maintain cursor shape on mode changes
+-- vim.api.nvim_create_autocmd("ModeChanged", {
+-- 	callback = function()
+-- 		vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250"
+-- 	end,
+-- 	desc = "Maintain cursor shape on mode change",
+-- })
 
 -- Enable indent guides for Python files
 vim.api.nvim_create_autocmd("FileType", {
